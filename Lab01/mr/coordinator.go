799a80ae1ce11141d8f7c2f6d56b7752745dcee3
Jaden Hums <jadenhums51@gmail.com>
2025-09-28 19:35:11 -0700

    have workers repeatedly call for tasks

--- Lab01/mr/coordinator.go
+++ Lab01/mr/coordinator.go
@@ -1,24 +1,27 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
 // Additional imports
-import "time"
-import "sync"
+
 //import "fmt"
 
 type Coordinator struct {
 	// Your definitions here.
 	nReduce int
 
-	workers map[int64]WorkerData
-	mapJobs []Job
+	workers    map[int64]WorkerData
+	mapJobs    []Job
 	reduceJobs []Job
-	mu sync.Mutex
+	mu         sync.Mutex
 
 	// "map", "reduce", "done"
 	phase string
@@ -59,38 +62,36 @@ type Job struct {
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-func (c *Coordinator) RegisterWorkerRPC(workerId int64, reply *GenericReply) error {
+func (c *Coordinator) RegisterWorkerRPC(args *WorkerArgs, reply *GenericReply) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	
+
 	// Add worker to list of workers
 	worker := WorkerData{}
 	worker.status = "idle"
 	worker.curJobStartTime = -1
 	worker.lastHeartbeat = time.Now().Unix()
-	c.workers[workerId] = worker
+	c.workers[args.WorkerId] = worker
 
 	reply.Success = true
 	return nil
 }
 
-func (c *Coordinator) HeartbeatRPC(workerID int64, reply *GenericReply) error {
+func (c *Coordinator) HeartbeatRPC(args *WorkerArgs, reply *GenericReply) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	newWorkerData := c.workers[workerID]
+	newWorkerData := c.workers[args.WorkerId]
 	newWorkerData.lastHeartbeat = time.Now().Unix()
-	c.workers[workerID] = newWorkerData
+	c.workers[args.WorkerId] = newWorkerData
 
 	reply.Success = true
 	return nil
@@ -103,13 +104,13 @@ func (c *Coordinator) HeartbeatRPC(workerID int64, reply *GenericReply) error {
 
 // side note: if we stick to this assigning logic, idk what use the worker status would be used for.
 // not even sure if what the heartbeat monitor is used for either, but it's there anyways
-func (c *Coordinator) Task(workerID int64, reply *TaskReply) error {
+func (c *Coordinator) Task(args *WorkerArgs, reply *TaskReply) error {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	
+
 	if c.phase == "map" {
-		for id, jobData:= range c.mapJobs {
-			if jobData.status == "incomplete"  || (jobData.status == "in progress" && time.Now().Unix() - jobData.jobStartTime > 10) {
+		for id, jobData := range c.mapJobs {
+			if jobData.status == "incomplete" || (jobData.status == "in progress" && time.Now().Unix()-jobData.jobStartTime > 10) {
 				reply.JobId = id
 				reply.File = jobData.file
 				reply.NReduce = c.nReduce
@@ -120,7 +121,7 @@ func (c *Coordinator) Task(workerID int64, reply *TaskReply) error {
 				c.mapJobs[id] = newJobData
 
 				return nil
-			} 
+			}
 		}
 
 		// No available tasks
@@ -133,19 +134,31 @@ func (c *Coordinator) Task(workerID int64, reply *TaskReply) error {
 	return nil
 }
 
+func (c *Coordinator) WorkerCompletion(args *CompletionArgs, reply *GenericReply) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
+	switch args.JobType {
+	case "map":
+		c.mapJobs[args.JobId].status = "complete"
+	case "reduce":
+		c.reduceJobs[args.JobId].status = "complete"
+	}
+	reply.Success = true
+	return nil
+}
 
 // Checks every second if any workers have crashed
 func (c *Coordinator) heartbeatMonitor() {
 	for {
 		c.mu.Lock()
-		
+
 		for workerId, workerData := range c.workers {
 			newWorkerData := workerData
-			if time.Now().Unix() - workerData.lastHeartbeat > 10 {
+			if time.Now().Unix()-workerData.lastHeartbeat > 10 {
 				// Worker has crashed
 				newWorkerData.status = "crashed"
-				
+
 			}
 			c.workers[workerId] = newWorkerData
 		}
@@ -161,15 +174,15 @@ func (c *Coordinator) mapTasksMonitor() {
 		c.mu.Lock()
 
 		done := true
-		for _, jobData:= range c.mapJobs {
+		for _, jobData := range c.mapJobs {
 			if jobData.status != "complete" {
 				done = false
 				break
-			} 
+			}
 		}
 
 		c.mu.Unlock()
-		
+
 		if done {
 			c.phase = "reduce"
 			return
@@ -179,9 +192,7 @@ func (c *Coordinator) mapTasksMonitor() {
 	}
 }
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -198,43 +209,40 @@ func (c *Coordinator) server() {
 func (c *Coordinator) initMapTasks(files []string) {
 	// Add each file as a job inside Coordinator
 	for _, file := range files {
-        //fmt.Println(file)
+		//fmt.Println(file)
 
 		job := Job{}
 		job.file = file
 		job.status = "incomplete"
 
 		c.mapJobs = append(c.mapJobs, job)
-    }
+	}
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	// Your code here.
 
 	return c.phase == "done"
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 	c.nReduce = nReduce
 	c.phase = "map"
 
-	// Your code here.
+	// Initialize the workers map
+	c.workers = make(map[int64]WorkerData)
 	c.initMapTasks(files)
 
 	// Monitors
 	go c.heartbeatMonitor()
 	go c.mapTasksMonitor()
 
-	c.server()	
+	c.server()
 	return &c
 }
--- Lab01/mr/worker.go
+++ Lab01/mr/worker.go
@@ -1,56 +1,75 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"time"
+)
 
 // Additional imports
-import "time"
 
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
 	workerId := time.Now().UnixNano()
-	CallRegister(workerId)
-
+	args := WorkerArgs{}
+	args.WorkerId = workerId
+	RegisterWorker(args)
 	go CallHeartbeat(workerId)
+
+	for {
+		reply := TaskReply{}
+
+		ok := call("Coordinator.Task", &args, &reply)
+		if !ok {
+			break
+		}
+
+		if reply.JobId == -1 {
+			time.Sleep((time.Second))
+			continue
+		}
+
+		compArgs := CompletionArgs{
+			JobId:     0,
+			JobType:   "map",
+			WorkderId: workerId,
+		}
+		compReply := GenericReply{}
+		call("Coordinator.WorkerCompletion", &compArgs, &compReply)
+		time.Sleep(time.Second)
+
+		// 3. If map task, execute map function
+		// 4. If reduce task, execute reduce function
+		// 5. report completion to coodinator in RPC call
+	}
 
 }
 
 // Ping the coordinator every second
 func CallHeartbeat(workerId int64) {
 	for {
+		args := WorkerArgs{}
 		reply := GenericReply{}
 
-		ok := call("Coordinator.HeartbeatRPC", workerId, &reply)
+		ok := call("Coordinator.HeartbeatRPC", &args, &reply)
 		if ok {
 
 		} else {
@@ -61,10 +80,10 @@ func CallHeartbeat(workerId int64) {
 	}
 }
 
-func CallRegister(workerId int64) {
+func RegisterWorker(args WorkerArgs) {
 	reply := GenericReply{}
 
-	ok := call("Coordinator.RegisterWorkerRPC", workerId, &reply)
+	ok := call("Coordinator.RegisterWorkerRPC", &args, &reply)
 	if ok {
 
 	} else {
@@ -72,11 +91,9 @@ func CallRegister(workerId int64) {
 	}
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -101,11 +118,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
